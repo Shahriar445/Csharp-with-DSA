22be803ee5fc4f953887b45b33c31451bb269f63
Shahrriar Haque <shahriarhaque445@gmail.com>
2024-06-04 12:04:11 +0600

    Binarry Search Recursive

---
+++ C# with DSA/Binarry_Search_Recursive.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__with_DSA
+{
+    public class Binarry_Search_Recursive
+    {
+        public int BinarrySearch(int[] number, int low, int high, int target)
+        {
+            if (low> high) return -1; // base case
+
+            int mid= (high+low)/2;
+
+            if (number[mid]==target) return mid;
+            else if (number[mid]< target)
+            {
+                return BinarrySearch(number, mid+1, high,target);
+            }
+            else
+            {
+              return   BinarrySearch(number,low,mid-1,target);
+            }
+              
+         }
+
+        int Searching(int[] number, int target)
+        {
+            // arr , lower element , upper element , target 
+            return BinarrySearch(number, 0, number.Length-1, target);
+        }
+
+
+        public void Main_BS()
+        {
+            Console.WriteLine("Enter the size of Array: ");
+            int size = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Enter sorted  {size} of Array :");
+            int[] arr = new int[size];
+            for (int i =0;i<arr.Length;i++)
+            {
+               arr[i]= int.Parse(Console.ReadLine());  // Array input 
+            }
+
+            Console.WriteLine("Enter The Target");
+            int target = int.Parse(Console.ReadLine()); // input target variable 
+            int result = Searching(arr, target); // called the Searching recursion method 
+
+            if (result == -1)
+            {
+                Console.WriteLine("Target Not Found");
+            }
+            else
+            {
+                Console.WriteLine("Target Found in index number: "+result);
+            }
+
+        }
+    }
+}
--- C# with DSA/Program.cs
+++ C# with DSA/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using C__with_DSA;
+
+Console.WriteLine("Binarry Searching Recursive method ");
+
+Binarry_Search_Recursive obj = new Binarry_Search_Recursive();
+
+
+obj.Main_BS();
