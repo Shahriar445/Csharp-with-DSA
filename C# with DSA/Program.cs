b853586335c177e1b96df557dcf70ec13e7fc2e2
Shahrriar Haque <shahriarhaque445@gmail.com>
2024-06-12 09:16:10 +0600

    Selection Sort added

--- C# with DSA/Program.cs
+++ C# with DSA/Program.cs
@@ -1,9 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 using C__with_DSA;
 
-Console.WriteLine("Binarry Searching Recursive method ");
+Console.WriteLine("Selection Sorting");
+SelectionSort selectionSort = new SelectionSort();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.WriteLine("Binary Searching Recursive method ");
 
 Binarry_Search_Recursive obj = new Binarry_Search_Recursive();
 
 
 obj.Main_BS();
+
---
+++ C# with DSA/SelectionSort.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__with_DSA
+{
+    public  class SelectionSort
+    {
+        public SelectionSort() {
+
+            int[] arr = { 7, 5, 9, 2, 8 };
+            int n = arr.Length;
+            int mini = 0;
+            int i = 0;
+            for (i = 0; i<n-1; i++)
+            {
+                 mini = i;
+
+                for (int j = i+1; j<n; j++)
+                {
+                    if (arr[j] < arr[mini])
+                    {
+                        mini= j;
+                    }
+                }
+                int temp = arr[mini];
+                arr[mini]= arr[i];
+                arr[i]= temp;
+            }
+           
+
+            Console.WriteLine("Sorted Array");
+            for ( i=0;i<n;i++)
+            {
+                Console.WriteLine(arr[i]+ " "); 
+            }
+
+            
+        
+        
+        }
+
+    }
+}
